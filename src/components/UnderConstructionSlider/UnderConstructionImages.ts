72f48cfe3386170b56b534b7a3b4033e137e3a1c
Leoni Lubbinge <tahoni@gmail.com>
2024-07-18 19:38:23 +0200

    Exclude all .env files from git

--- src/components/UnderConstructionSlider/UnderConstructionImages.ts
+++ src/components/UnderConstructionSlider/UnderConstructionImages.ts
@@ -1,12 +1,12 @@
-import {ImageSourceDescription} from "@tahoni/tahoni-lib-react";
+import {ImageWithSourceAndDescription} from "@tahoni/tahoni-lib-react";
 import ladyShooterOnIpscRangeImage from './images/lady-shooter-on-ipsc-range.png';
 import ipscTargetWithBulletHoles from './images/ipsc-target-with-bullet-holes.png';
 
 export class UnderConstructionImages {
-    static underConstructionLadyImage: ImageSourceDescription =
+    static underConstructionLadyImage: ImageWithSourceAndDescription =
         {image: ladyShooterOnIpscRangeImage,
             description: 'Lady shooter on IPSC range'};
-    static underConstructionTargetImage: ImageSourceDescription =
+    static underConstructionTargetImage: ImageWithSourceAndDescription =
         {image: ipscTargetWithBulletHoles,
             description: 'IPSC target with bullet holes'};
 }
