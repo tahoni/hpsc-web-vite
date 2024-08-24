31377cf15a05b0b0f6db3dcb12b3b520900f0c52
Leoni Lubbinge <tahoni@gmail.com>
2024-08-24 02:37:48 +0200

    Put back the Under Construction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import {ReactElement} from "react";
 import {Route, Routes} from "react-router";
 import {Breakpoints} from "./layout/Breakpoints";
-import {Layout} from "./layout";
-import {HomePage} from "./pages/HomePage";
+import {UnderConstructionPage} from "./pages/UnderConstructionPage";
 import './App.scss'
 
 function App(): ReactElement {
   return (
       <>
           <Routes>
-              <Route element={<Layout/>}>
-                  <Route path="/" element={<HomePage/>}/>
-                  <Route path="*" element={<HomePage/>}/>
+              <Route element={<UnderConstructionPage/>}>
+                  <Route path="/" element={<UnderConstructionPage/>}/>
+                  <Route path="*" element={<UnderConstructionPage/>}/>
               </Route>
           </Routes>
           <Breakpoints/>
