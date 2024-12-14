62a1d598729626576fcdf5af3aba4358cb5be4cf
Leoni Lubbinge <tahoni@gmail.com>
2024-12-14 19:04:18 +0200

    Working home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import {ReactElement} from "react";
 import {Route, Routes} from "react-router";
 import {Breakpoints} from "./layout/Breakpoints";
-import {UnderConstructionPage} from "./pages/UnderConstructionPage";
+import {Layout} from "./layout";
+import {HomePage} from "./pages/Home";
 import './App.scss'
 
 function App(): ReactElement {
-  return (
-      <>
-          <Routes>
-              <Route element={<UnderConstructionPage/>}>
-                  <Route path="/" element={<UnderConstructionPage/>}/>
-                  <Route path="*" element={<UnderConstructionPage/>}/>
-              </Route>
-          </Routes>
-          <Breakpoints/>
-      </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route element={<Layout/>}>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/home" element={<HomePage/>}/>
+                    {/*<Route path="/contacts" element={<Contacts/>}/>*/}
+                    {/*<Route path="/faq" element={<Faq/>}/>*/}
+                    {/*<Route path="/how_to_join" element={<HowToJoinPage/>}/>*/}
+                    {/*<Route path="/venues" element={<Venues/>}/>*/}
+                    <Route path="*" element={<HomePage/>}/>
+                </Route>
+            </Routes>
+            <Breakpoints/>
+        </>
+    )
 }
 
 export default App;
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,13 +1,18 @@
 import {ReactElement} from "react";
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Image, Row} from "react-bootstrap";
+import HPSCLogo from "../../assets/images/logos/hpsc-logo.png";
+import NGPSALogo from "../../assets/images/logos/ngpsa-logo.png"
 import classes from "./Header.module.scss";
+import {HeaderContent} from "./HeaderContent.tsx";
 
 export const Header = (): ReactElement => {
     return (
-        <Container fluid className={classes.header}>
+        <Container fluid className={classes.main}>
             <Row>
                 <Col>
-
+                    <Image src={HPSCLogo} alt="HPSC logo" height={128} width={128} className={classes.hpscLogo}/>
+                    <HeaderContent title="Hartbeespoortdam Practical Shooting Club"/>
+                    <Image src={NGPSALogo} alt="NGPSA logo" height={128} width={128} className={classes.ngpsaLogo}/>
                 </Col>
             </Row>
         </Container>
