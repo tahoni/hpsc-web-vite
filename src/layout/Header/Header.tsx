cbdd86d5140916eaf4c87cf6962e81a7a75a8ce0
Leoni Lubbinge <tahoni@gmail.com>
2024-12-15 11:05:30 +0200

    Improve the alignment of the header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -10,17 +10,14 @@ export const Header = (): ReactElement => {
         <Container fluid className={classes.this}>
             <Row>
                 <Col xs={{span: 6}} md={{span: 2}}
-                     lg={{span: 1}} xl={{span: 1, offset: 1}}
                      className={classes.logo}>
                     <Image src={HPSCLogo} alt="HPSC logo" className={classes.hpscLogo}/>
                 </Col>
                 <Col xs={{span: 12, order: 'last'}} md={{span: 8}}
-                     lg={{span: 8, offset: 1}} xl={{span: 8, offset: 0}}
                      className={classes.inner}>
                     <HeaderContent title="Hartbeespoortdam Practical Shooting Club"/>
                 </Col>
                 <Col xs={{span: 6}} md={{span: 2, order: 'last'}}
-                     lg={{span: 1, offset: 1}} xl={{span: 1, offset: 0}}
                      className={classes.logo}>
                     <Image src={NGPSALogo} alt="NGPSA logo" className={classes.ngpsaLogo}/>
                 </Col>
