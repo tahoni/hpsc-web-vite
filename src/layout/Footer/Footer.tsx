3d4b9ee9013592979f4b0fb2874c8dd0f70e0af9
Leoni Lubbinge <tahoni@gmail.com>
2024-12-18 00:29:40 +0200

    Add the map back in the footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import {ReactElement} from "react";
 import {Col, Container, Row} from "react-bootstrap";
+import {ShootingRangeMap} from "../../components";
 import classes from "./Footer.module.scss"
 
 export const Footer = (): ReactElement => {
@@ -7,7 +8,7 @@ export const Footer = (): ReactElement => {
         <Container fluid className={classes.Footer}>
             <Row className={classes.FooterInner}>
                 <Col>
-                    {/*<ShootingRangeMap/>*/}
+                    <ShootingRangeMap/>
                 </Col>
             </Row>
         </Container>
