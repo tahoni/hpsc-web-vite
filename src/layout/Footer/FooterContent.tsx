0d495daf8098ccfcb484e28ad594cc01e2a0ae73
Leoni Lubbinge <tahoni@gmail.com>
2024-12-24 14:04:45 +0200

    Align the map in the footer centrally

--- src/layout/Footer/FooterContent.tsx
+++ src/layout/Footer/FooterContent.tsx
@@ -1,31 +1,36 @@
-import React, {ReactElement} from "react";
+import React, {CSSProperties, ReactElement} from "react";
 import {Col, Row} from "react-bootstrap";
 import {SimpleVenueMap} from "../../components";
 import {VenueType} from "../../model/VenueTypes.ts";
-import {mapHeight, mapWidth} from "../../constants/MapConstants.ts";
 import {EUFEES_RANGE, rangeMapPins} from "../../constants/content/Ranges.ts";
 import classes from "./Footer.module.scss"
 
 export const FooterContent = React.memo(
     (): ReactElement => {
 
-    const footerMapRange: string = EUFEES_RANGE;
-    const footerMapPins: VenueType[] = [
-        rangeMapPins.get(footerMapRange),
-    ]
+        const mapStyle: CSSProperties = {
+            width: classes.mapWidth,
+            height: classes.mapHeight,
+        }
 
-    return (
-        <Row className={classes.FooterContent}>
-            <Col xs={{span: 12}} md={{span: 6}} className={classes.FooterMap}>
-                <SimpleVenueMap
-                    mapStyle={{width: mapWidth, height: mapHeight}}
-                    center={rangeMapPins.get(footerMapRange)?.latLng}
-                    venues={footerMapPins}
-                />
-            </Col>
-            <Col className={classes.FooterText}>
+        const footerMapRange: string = EUFEES_RANGE;
+        const footerMapPins: VenueType[] = [
+            rangeMapPins.get(footerMapRange),
+        ]
 
-            </Col>
-        </Row>
-    )
-})
+        return (
+            <Row className={classes.FooterContent}>
+                <Col xs={{span: 12}} md={{span: 6}} className={classes.FooterMap}>
+                    <SimpleVenueMap
+                        mapStyle={mapStyle}
+                        center={rangeMapPins.get(footerMapRange)?.latLng}
+                        venues={footerMapPins}
+                    />
+                </Col>
+                <Col className={classes.FooterText}>
+
+                </Col>
+            </Row>
+        )
+    }
+)
