0d60af2db64eb02b7d28a832a3c55956b58514cc
Leoni Lubbinge <tahoni@gmail.com>
2024-12-30 18:56:57 +0200

    Use the History page as the Home page so long

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,12 +1,43 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { HomeContent } from "../../content";
+import { HistoryContent } from "../../content";
+import {
+  clubAbbreviation,
+  clubName,
+  clubShootingRange,
+} from "../../constants/about/ClubConstants.ts";
+import {
+  internationalAssociationAbbreviation,
+  internationalAssociationName,
+  internationalAssociationWebsite,
+  nationalAssociationAbbreviation,
+  nationalAssociationName,
+  nationalAssociationWebsite,
+  provincialAssociationAbbreviation,
+  provincialAssociationName,
+  provincialAssociationWebsite,
+} from "../../constants/about/AssociationConstants.ts";
 
 export const HomePage = React.memo(() => {
   return (
     <Row>
       <Col>
-        <HomeContent />
+        <HistoryContent
+          clubName={clubName}
+          clubAbbreviation={clubAbbreviation}
+          clubShootingRage={clubShootingRange}
+          internationalAssociationWebsite={internationalAssociationWebsite}
+          internationalAssociationName={internationalAssociationName}
+          internationalAssociationAbbreviation={
+            internationalAssociationAbbreviation
+          }
+          nationalAssociationWebsite={nationalAssociationWebsite}
+          nationalAssociationName={nationalAssociationName}
+          nationalAssociationAbbreviation={nationalAssociationAbbreviation}
+          provincialAssociationWebsite={provincialAssociationWebsite}
+          provincialAssociationName={provincialAssociationName}
+          provincialAssociationAbbreviation={provincialAssociationAbbreviation}
+        />
       </Col>
     </Row>
   );
