046f1e0818f0f994aae4bcaae7731169bdb7b998
Leoni Lubbinge <tahoni@gmail.com>
2025-01-03 19:38:20 +0200

    Wrap the CAPTCHA components in React.memo

--- src/components/Captcha/SimpleCaptcha.tsx
+++ src/components/Captcha/SimpleCaptcha.tsx
@@ -1,25 +1,31 @@
-import { ReactElement } from "react";
+import React, { ReactElement } from "react";
 import { ReCaptchaV2, TReCaptchaV2Callback } from "react-recaptcha-x";
 
 interface SimpleCaptchaProps {
   onChange?: (token?: string) => void;
 }
 
-export const SimpleCaptcha = (props: SimpleCaptchaProps): ReactElement => {
-  const v2Callback: TReCaptchaV2Callback = (
-    token: string | false | Error,
-  ): void => {
-    let value: string = "";
-    if (typeof token === "string") {
-      value = token;
-    }
+export const SimpleCaptcha = React.memo(
+  (props: SimpleCaptchaProps): ReactElement => {
+    const v2Callback: TReCaptchaV2Callback = (
+      token: string | false | Error,
+    ): void => {
+      let value: string = "";
+      if (typeof token === "string") {
+        value = token;
+      }
 
-    if (props.onChange !== undefined) {
-      props.onChange(value);
-    }
-  };
+      if (props.onChange !== undefined) {
+        props.onChange(value);
+      }
+    };
 
-  return (
-    <ReCaptchaV2 className="simpleCaptcha" callback={v2Callback} tabindex={0} />
-  );
-};
+    return (
+      <ReCaptchaV2
+        className="simpleCaptcha"
+        callback={v2Callback}
+        tabindex={0}
+      />
+    );
+  },
+);
