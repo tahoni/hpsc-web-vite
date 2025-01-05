c612a1b8e22d14643c987b95cb84a9635907064f
Leoni Lubbinge <tahoni@gmail.com>
2025-01-05 09:25:20 +0200

    Remove the problematic CAPTCHA dependency

--- src/components/Captcha/SimpleCaptcha.tsx
+++ src/components/Captcha/SimpleCaptcha.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from "react";
-import { ReCaptchaV2, TReCaptchaV2Callback } from "react-recaptcha-x";
-import classes from "./Captcha.module.scss";
+// import { ReCaptchaV2, TReCaptchaV2Callback } from "react-recaptcha-x";
+// import classes from "./Captcha.module.scss";
 
 interface SimpleCaptchaProps {
   onChange?: (token?: string) => void;
@@ -8,25 +8,31 @@ interface SimpleCaptchaProps {
 
 export const SimpleCaptcha = React.memo(
   (props: SimpleCaptchaProps): ReactElement => {
-    const v2Callback: TReCaptchaV2Callback = (
-      token: string | false | Error,
-    ): void => {
-      let value: string = "";
-      if (typeof token === "string") {
-        value = token;
-      }
-
-      if (props.onChange !== undefined) {
-        props.onChange(value);
-      }
-    };
+    /*
+        const v2Callback: TReCaptchaV2Callback = (
+          token: string | false | Error,
+        ): void => {
+          let value: string = "";
+          if (typeof token === "string") {
+            value = token;
+          }
+    
+        const value: string = "captcha";
+          if (props.onChange !== undefined) {
+            props.onChange(value);
+          }
+        };
+    */
 
     return (
-      <ReCaptchaV2
-        className={classes.simpleCaptcha}
-        callback={v2Callback}
-        tabindex={0}
-      />
+      <></>
+      /*
+            <ReCaptchaV2
+              className={classes.simpleCaptcha}
+              callback={v2Callback}
+              tabindex={0}
+            />
+      */
     );
   },
 );
