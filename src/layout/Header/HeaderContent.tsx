8098d7801366f1825bf59232fb399ca148b41600
Leoni Lubbinge <tahoni@gmail.com>
2025-01-05 08:46:49 +0200

    Format padding changes. Add padding around items in the About Us form. Add padding in the header content. Wrap the footer content with <></>

--- src/layout/Header/HeaderContent.tsx
+++ src/layout/Header/HeaderContent.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import classes from "./Header.module.scss";
 
 interface HeaderContentProps {
@@ -9,11 +9,17 @@ interface HeaderContentProps {
 export const HeaderContent = React.memo(
   (props: HeaderContentProps): ReactElement => {
     return (
-      <Row className={classes.headerContent}>
-        <Col className={classes.headerText}>
-          <h1>{props.title}</h1>
-        </Col>
-      </Row>
+      <Container fluid className={classes.headerContent}>
+        <Row className={classes.headerItem}>
+          <Col className={classes.headerText}>
+            <h1>{props.title}</h1>
+          </Col>
+        </Row>
+        <Row className={classes.headerItem}>
+          <Col>
+          </Col>
+        </Row>
+      </Container>
     );
   },
 );
