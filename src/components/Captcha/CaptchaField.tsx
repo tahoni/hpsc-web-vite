90b08bf85b400c7594c7db853bd42d693d5243b1
Leoni Lubbinge <tahoni@gmail.com>
2025-02-27 09:57:54 +0200

    Remove the reCAPCHAR library that is currently used

--- src/components/Captcha/CaptchaField.tsx
+++ src/components/Captcha/CaptchaField.tsx
@@ -1,14 +1,15 @@
 import React, { ReactElement } from "react";
-import { FieldProps } from "@rjsf/utils";
 import { SimpleCaptcha } from "./SimpleCaptcha";
 
 const CaptchaField = React.memo(
-  (props: FieldProps): ReactElement => {
-    const handleChange = (token?: string) => {
-      props.onChange(token !== undefined && token !== "");
-    };
+  (/*props: FieldProps*/): ReactElement => {
+    /*
+        const handleChange = (token?: string) => {
+          props.onChange(token !== undefined && token !== "");
+        };
+    */
 
-    return <SimpleCaptcha onChange={handleChange} />;
+    return <SimpleCaptcha />;
   }
 );
 
