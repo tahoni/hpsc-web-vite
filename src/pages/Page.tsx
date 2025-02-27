fd69c5793e027c4bd90c888c5078d74ad97bcdeb
Leoni Lubbinge <tahoni@gmail.com>
2025-02-27 09:56:47 +0200

    Put loader back.

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,6 +1,7 @@
-import React, { PropsWithChildren, ReactElement } from "react";
+import React, { PropsWithChildren, ReactElement, Suspense } from "react";
 import { Col, Row } from "react-bootstrap";
 import PageTitle from "../components/Title/PageTitle.tsx";
+import { Loader } from "@tahoni/tahoni-lib-react";
 
 interface PageProps {
   title: string;
@@ -19,11 +20,13 @@ const Page = React.memo((props: PropsWithChildren<PageProps>): ReactElement => {
         :
         <></>
       }
-      <Row>
-        <Col>
-          {props.children}
-        </Col>
-      </Row>
+      <Suspense fallback={<Loader isLoading={true} />} key={props.keyValue}>
+        <Row>
+          <Col>
+            {props.children}
+          </Col>
+        </Row>
+      </Suspense>
     </>
   );
 });
