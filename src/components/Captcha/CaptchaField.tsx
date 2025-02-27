5fc3ff3031f4b6504f99c8cd5a719bd9fcf7a6fe
Leoni Lubbinge <tahoni@gmail.com>
2025-02-27 10:45:01 +0200

    Revert "Remove the reCAPCHAR library that is currently used"
    This reverts commit 90b08bf85b400c7594c7db853bd42d693d5243b1.

--- src/components/Captcha/CaptchaField.tsx
+++ src/components/Captcha/CaptchaField.tsx
@@ -1,15 +1,14 @@
 import React, { ReactElement } from "react";
+import { FieldProps } from "@rjsf/utils";
 import { SimpleCaptcha } from "./SimpleCaptcha";
 
 const CaptchaField = React.memo(
-  (/*props: FieldProps*/): ReactElement => {
-    /*
-        const handleChange = (token?: string) => {
-          props.onChange(token !== undefined && token !== "");
-        };
-    */
+  (props: FieldProps): ReactElement => {
+    const handleChange = (token?: string) => {
+      props.onChange(token !== undefined && token !== "");
+    };
 
-    return <SimpleCaptcha />;
+    return <SimpleCaptcha onChange={handleChange} />;
   }
 );
 
--- src/components/Captcha/SimpleCaptcha.tsx
+++ src/components/Captcha/SimpleCaptcha.tsx
@@ -1,25 +1,30 @@
 import React, { ReactElement } from "react";
+import { reCaptchaV2SiteKey } from "../../constants/CaptchaConstants.ts";
+import classes from "./Captcha.module.scss";
 
-/*
 interface SimpleCaptchaProps {
   onChange?: (token?: string) => void;
 }
-*/
+
+const ReCAPTCHA = React.lazy(() => import("react-google-recaptcha"));
 
 export const SimpleCaptcha = React.memo(
-  (/*props: SimpleCaptchaProps*/): ReactElement => {
-    /*
-        const onChange = (token: string | null) => {
-          const value: string | undefined = token !== null ? token : undefined;
+  (props: SimpleCaptchaProps): ReactElement => {
+    const onChange = (token: string | null) => {
+      const value: string | undefined = token !== null ? token : undefined;
 
-          if (props.onChange !== undefined) {
-            props.onChange(value);
-          }
-        };
-    */
+      if (props.onChange !== undefined) {
+        props.onChange(value);
+      }
+    };
 
     return (
-      <></>
+      <ReCAPTCHA
+        className={classes.simpleCaptcha}
+        sitekey={reCaptchaV2SiteKey}
+        onChange={onChange}
+        hl="en"
+      />
     );
   }
 );
