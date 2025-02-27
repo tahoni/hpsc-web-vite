b5d2e903d2f420e6825504fb94d12b293cf1e20d
Leoni Lubbinge <tahoni@gmail.com>
2025-02-27 07:15:03 +0200

    Add the base URL

--- src/constants/AppConstants.ts
+++ src/constants/AppConstants.ts
@@ -1,3 +1,5 @@
 export const copyrightYear: number = 2025;
 
-export const textFontName = "Noto Sans";
+export const textFontName: string = "Noto Sans";
+
+export const baseUrl: string = import.meta.env.VITE_BASE_URL ?? "https://hpsc.co.za";
