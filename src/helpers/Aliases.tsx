683f5f42b1e328a9b3270f93f6c320fd42a57df4
Leoni Lubbinge <tahoni@gmail.com>
2025-04-26 15:04:10 +0200

    Return status 404 for page not found

--- src/helpers/Aliases.tsx
+++ src/helpers/Aliases.tsx
@@ -1,61 +1,85 @@
 import React from "react";
 import { PageMapping } from "../model/PageMapping.ts";
 import HomePage from "../pages/Home/HomePage.tsx";
-import PageNotFound from "../pages/NotFound/PageNotFound.tsx";
 import Page from "../pages/Page.tsx";
 import VenuesPage from "../pages/Venues/VenuesPage.tsx";
 
-const MembersPage = React.lazy(() => import("../pages/Members/MembersPage.tsx"));
+const MembersPage = React.lazy(
+  () => import("../pages/Members/MembersPage.tsx"),
+);
 const LinksPage = React.lazy(() => import("../pages/Links/LinksPage.tsx"));
-const HistoryPage = React.lazy(() => import("../pages/History/HistoryPage.tsx"));
-const ContactUsPage = React.lazy(() => import("../pages/ContactUs/ContactUsPage.tsx"));
-const AboutUsPage = React.lazy(() => import("../pages/AboutUs/AboutUsPage.tsx"));
+const HistoryPage = React.lazy(
+  () => import("../pages/History/HistoryPage.tsx"),
+);
+const ContactUsPage = React.lazy(
+  () => import("../pages/ContactUs/ContactUsPage.tsx"),
+);
+const AboutUsPage = React.lazy(
+  () => import("../pages/AboutUs/AboutUsPage.tsx"),
+);
 
 export const Home: PageMapping = {
   name: "Home",
   path: "/",
-  element: <Page keyValue={"homePage"} title={""}><HomePage /></Page>
-};
-export const NotFound: PageMapping = {
-  name: "Not Found",
-  path: "*",
-  element: <Page keyValue={"notFoundPage"}
-                 title={"Not Fond"}><PageNotFound /></Page>
+  element: (
+    <Page keyValue={"homePage"} title={""}>
+      <HomePage />
+    </Page>
+  ),
 };
 
 export const Members: PageMapping = {
   name: "Members",
   path: "/members",
-  element: <Page keyValue={"membersPage"}
-                 title={"Members"}><MembersPage /></Page>
+  element: (
+    <Page keyValue={"membersPage"} title={"Members"}>
+      <MembersPage />
+    </Page>
+  ),
 };
 export const Links: PageMapping = {
   name: "Links",
   path: "/links",
-  element: <Page keyValue={"linksPage"} title={"Links"}><LinksPage /></Page>
+  element: (
+    <Page keyValue={"linksPage"} title={"Links"}>
+      <LinksPage />
+    </Page>
+  ),
 };
 export const History: PageMapping = {
   name: "History",
   path: "/history",
-  element: <Page keyValue={"historyPage"}
-                 title={"History"}><HistoryPage /></Page>
+  element: (
+    <Page keyValue={"historyPage"} title={"History"}>
+      <HistoryPage />
+    </Page>
+  ),
 };
 export const AboutUs: PageMapping = {
   name: "About Us",
   path: "/about",
-  element: <Page keyValue={"aboutUsPage"}
-                 title={"About Us"}><AboutUsPage /></Page>
+  element: (
+    <Page keyValue={"aboutUsPage"} title={"About Us"}>
+      <AboutUsPage />
+    </Page>
+  ),
 };
 export const ContactUs: PageMapping = {
   name: "Contact Us",
   path: "/contact",
-  element: <Page keyValue={"contactUsPage"}
-                 title={"Contact Us"}><ContactUsPage /></Page>
+  element: (
+    <Page keyValue={"contactUsPage"} title={"Contact Us"}>
+      <ContactUsPage />
+    </Page>
+  ),
 };
 
 export const Venues: PageMapping = {
   name: "Shooting Ranges",
   path: "/venues",
-  element: <Page keyValue={"venuesPage"}
-                 title={"Shooting Ranges"}><VenuesPage /></Page>
+  element: (
+    <Page keyValue={"venuesPage"} title={"Shooting Ranges"}>
+      <VenuesPage />
+    </Page>
+  ),
 };
