3da547ecb8e3d7c41dde6a3284320d906869663a
Leoni Lubbinge <tahoni@gmail.com>
2025-06-01 10:15:45 +0200

    Add the sitemap to the root of the website

--- src/model/SitemapMapping.ts
+++ src/model/SitemapMapping.ts
@@ -1,14 +1,10 @@
 import { SitemapChangeFrequency } from "../enums/SitemapChangeFrequency";
-import { toUTCDateString } from "../utils/DateUtils";
-
-// const DEFAULT_SITEMAP_URL_CHANGEFREQ = SitemapChangeFrequency.Daily;
-// const DEFAULT_SITEMAP_URL_PRIORITY = 0.5;
 
 export class SitemapMapping {
   url: string;
   changefreq?: SitemapChangeFrequency;
   priority?: number;
-  lastmod?: string;
+  lastmod?: Date;
 
   constructor(mapping: {
     url: string;
@@ -17,12 +13,8 @@ export class SitemapMapping {
     lastmod?: Date;
   }) {
     this.url = mapping.url;
-    this.changefreq = mapping.changefreq ?? undefined;
-    this.priority = mapping.priority ?? undefined;
-
-    const lastmod: Date | undefined = mapping.lastmod;
-    if (lastmod) {
-      this.lastmod = toUTCDateString(lastmod);
-    }
+    this.changefreq = mapping.changefreq;
+    this.priority = mapping.priority;
+    this.lastmod = mapping.lastmod;
   }
 }
